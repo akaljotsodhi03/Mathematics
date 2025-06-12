d48d5cc9de16c684c53c8b1fbdf2ddcd7fe7fec8
akaljotsodhi03 <sodha2@student.op.ac.nz>
2025-06-12 23:36:47 +1200

    Logic Gates 2

---
+++ Gates2/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Gates_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter value for Top input (0 or 1): ");
+            int top = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter value for Middle input (0 or 1): ");
+            int middle = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter value for Bottom input (0 or 1): ");
+            int bottom = Convert.ToInt32(Console.ReadLine());
+
+            // First OR gate: top OR middle
+            int or1 = top | middle;
+
+            // Second OR gate: middle OR bottom
+            int or2 = middle | bottom;
+
+            // NOT gate: invert or2
+            int notOr2;
+            if (or2 == 0)
+                notOr2 = 1;
+            else
+                notOr2 = 0;
+
+            // AND gate: or1 AND notOr2
+            int x = or1 & notOr2;
+
+            Console.WriteLine("X = " + x);
+        }
+    }
+}
